4ab40c6be20a6235e905aaa0e982c627cd4c2b77
Dikshapal-git <dikshapal3196@gmail.com>
2023-11-03 15:16:19 +0530

    msg

---
+++ src/main/java/com/standalone1/App.java
@@ -0,0 +1,13 @@
+package com.standalone1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
